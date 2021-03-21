9eef6fc4a02602045e4b053601c958ad5fa886bc
yankihue <yankisirtas@hotmail.com>
2021-03-21 18:51:01 +0300

    Initial commit

---
+++ votingapp/schema.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    username: str
+
+class UserCreate(UserBase):
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+
+class User(UserBase):
+    id: int
+    
+    class Config:
+        orm_mode = True
+        
