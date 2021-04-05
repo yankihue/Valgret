b1c62ac47ef9eff9ff58ee574e1ea818ac5fdd90
yankihue <yankisirtas@hotmail.com>
2021-04-06 00:20:10 +0300

    Remove unnecessary BASE_DIR

--- votingapp/database.py
+++ votingapp/database.py
@@ -6,7 +6,6 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 # this line is to connect to our base dir and connect to our .env file
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(find_dotenv())
 
 # this is to access the db so any route can acccess the database session
