46b3288100fba6ebb218aff583804c46d9d75ab3
yankihue <yankisirtas@hotmail.com>
2021-04-06 21:51:25 +0300

    Change lazyload type for models

--- votingapp/model.py
+++ votingapp/model.py
@@ -12,20 +12,8 @@ class User(Base):
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
-    items = relationship("Item", back_populates="owner", lazy="noload")
-    elections = relationship("Election", back_populates="owner", lazy="noload")
-    ballots = relationship("Ballot", back_populates="owner", lazy="noload")
-
-
-class Item(Base):
-    __tablename__ = "items"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="items")
+    elections = relationship("Election", back_populates="owner", lazy="subquery")
+    ballots = relationship("Ballot", back_populates="owner", lazy="subquery")
 
 
 class Election(Base):
@@ -37,17 +25,20 @@ class Election(Base):
     owner_id = Column(Integer, ForeignKey("users.id"))
     created_date = Column(DateTime(timezone=True), server_default=func.now())
     owner = relationship("User", back_populates="elections")
-    candidates = relationship("Candidate", back_populates="owner", lazy="noload")
-    ballots = relationship("Ballot", back_populates="election", lazy="noload")
+    candidates = relationship("Candidate", back_populates="owner", lazy="subquery")
+    ballots = relationship("Ballot", back_populates="election", lazy="subquery")
 
 
+# TODO: changed lazyload type from lazy="noload" to lazy="subquery"
+# fixes the empty child object list upon parent object creation
+# but still not sure if its problematic
 class Candidate(Base):
     __tablename__ = "candidates"
 
     id = Column(Integer, primary_key=True, index=True)
     info = Column(String, index=True)
     election_id = Column(Integer, ForeignKey("elections.id"))
-    owner = relationship("Election", back_populates="candidates", lazy="noload")
+    owner = relationship("Election", back_populates="candidates")
 
 
 class Ballot(Base):
@@ -59,4 +50,3 @@ class Ballot(Base):
     owner_id = Column(Integer, ForeignKey("users.id"))
     owner = relationship("User", back_populates="ballots")
     election = relationship("Election", back_populates="ballots")
-
--- votingapp/schema.py
+++ votingapp/schema.py
@@ -4,22 +4,6 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
-class UserBase(BaseModel):
-    email: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-
-    class Config:
-        orm_mode = True
-
-
 class CandidateBase(BaseModel):
     info: str
 
@@ -56,6 +40,23 @@ class Election(ElectionBase):
     created_date: datetime
     candidates: List[Candidate] = []
     ballots: List[Ballot] = []
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    elections: List[Election] = []
 
     class Config:
         orm_mode = True
